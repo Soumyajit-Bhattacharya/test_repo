076137a23b5c8c4c9b7d1b38d7cd5d22fd135a5e
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-04-26 10:24:54 +0530

    updated settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#2m+w_wcnm@+no6wt6&gcspgs_3o+@%e(azni7sitw4p(y9nv#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','server ip']
 
 
 # Application definition
